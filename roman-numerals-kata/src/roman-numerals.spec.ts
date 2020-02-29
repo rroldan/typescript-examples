09e0806c9f5d23159f59f13f12b8478b307c06cc
rroldan <roldan.ricardo@gmail.com>
2020-02-29 20:07:04 +0100

    convert number to ten

--- roman-numerals-kata/src/roman-numerals.spec.ts
+++ roman-numerals-kata/src/roman-numerals.spec.ts
@@ -10,7 +10,19 @@ describe('convert decimal number to roman number', () => {
   test('convert 3 to III', () => {
     expect(convert(3)).toMatch('III');
   })
+  test('convert 4 to IV', () => {
+    expect(convert(4)).toMatch('IV');
+  })
   test('convert 5 to V', () => {
     expect(convert(5)).toMatch('V');
+  })
+  test('convert 6 to VI', () => {
+    expect(convert(6)).toMatch('VI');
+  })
+  test('convert 9 to IX', () => {
+    expect(convert(9)).toMatch('IX');
+  })
+  test('convert 10 to X', () => {
+    expect(convert(10)).toMatch('X');
   })
 });
--- roman-numerals-kata/src/roman-numerals.ts
+++ roman-numerals-kata/src/roman-numerals.ts
@@ -5,6 +5,10 @@
          return "I";
      }
 
+     if ( decimalNumber === 4) {
+         return "IV";
+     }
+
      if (decimalNumber < 5){
          return convert(1) + convert(decimalNumber - 1);
      }
@@ -12,5 +16,18 @@
      if (decimalNumber === 5){
          return "V";
      }
+
+    if ( decimalNumber === 9) {
+        return "IX";
+    }
+
+     if (decimalNumber > 5 && decimalNumber < 10){
+         return convert(5) + convert(decimalNumber -5)
+     }
+
+     if (decimalNumber === 10) {
+         return "X";
+     }
+
 }
 
